c54d224d27fc5c6ea267b4baa125e3ac193f8c16
= <vincent.qi@hotmail.com>
2021-05-09 12:37:35 -0700

    Updated PlayerListener

--- src/eventlisteners/PlayerListener.java
+++ src/eventlisteners/PlayerListener.java
@@ -5,6 +5,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -24,11 +26,22 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getInventory().getType() != InventoryType.PLAYER) return;
+
         if (event.getSlot() > 35 && event.getSlot() < 45) {
-            Bukkit.broadcastMessage("CLICK CLEARED");
-            event.setCancelled(true);
 
             // TODO: Individual functions
+            if (event.getSlot() == 41) {
+                Bukkit.broadcastMessage("CLICKED SLOT 41");
+            }
+
+            Bukkit.broadcastMessage("CLICK CLEARED");
+            event.setCancelled(true);
         }
+    }
+
+    public void onInventoryDrag(InventoryDragEvent event) {
+        if (event.getInventory().getType() != InventoryType.PLAYER) return;
+        // TODO: Fix dragging
     }
 }
